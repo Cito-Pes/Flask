33b407531a014931f4e1f79b960e859624ec634e
choijino <citopes@gmail.com>
2024-01-14 20:16:20 +0900

    Marge_PDF Add

---
+++ PDF/Merge_PDF.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template, send_file
+from PyPDF2 import PdfFileMerger
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('./PDF/Merge_PDF.html')
+
+
+@app.route('/merge', methods=['POST'])
+def merge_pdf():
+    files = request.files.getlist('pdf_files')
+    filename = request.form['pdf_filename']
+
+    merger = PdfFileMerger()
+    for file in files:
+        merger.append(file)
+
+    merged_file_path = f'{filename}.pdf' if filename else 'merged.pdf'
+    merger.write(merged_file_path)
+    merger.close()
+
+    return render_template('./PDF/Merge_PDF_Down.html', filename=merged_file_path)
+
+
+@app.route('/download/<filename>')
+def download_pdf(filename):
+    return send_file(filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run()
